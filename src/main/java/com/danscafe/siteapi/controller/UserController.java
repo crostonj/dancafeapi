36399df2cec6b58452748a5e2e5ca980c45b8ead
crostonj <Fl@gator5>
2020-05-09 11:46:09 -0600

    Conversion to Java

---
+++ src/main/java/com/danscafe/siteapi/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.danscafe.siteapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping({"/hello"})
+    public String hello(){
+        return "Hello";
+    }
+}
