c5fc27f0308c9e00329bf733c8b7e2f746cf3192
crostonj <Fl@gator5>
2020-05-09 10:19:04 -0600

    Comversion to Java

---
+++ src/test/java/com/danscafe/siteapi/SiteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danscafe.siteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
